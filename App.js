0782ed4b2e6197f32e5a5be98824a9e388ac2287
Chandler Roth <hey@chandlerroth.com>
2021-04-14 10:58:03 -0400

    Add permissions package and simplify audio connection

--- App.js
+++ App.js
@@ -1,21 +1,155 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import {View, SafeAreaView, Button, StyleSheet} from 'react-native';
+
+import {RTCPeerConnection, mediaDevices} from 'react-native-webrtc';
 
 export default function App() {
+  const [localStream, setLocalStream] = React.useState();
+  const [remoteStream, setRemoteStream] = React.useState();
+  const [cachedLocalPC, setCachedLocalPC] = React.useState();
+  const [cachedRemotePC, setCachedRemotePC] = React.useState();
+  const [microphonePermission, askMicrophonePermission, getMicrophonePermission] = Permissions.usePermissions(Permissions.AUDIO_RECORDING, { ask: true });
+
+  const [isMuted, setIsMuted] = React.useState(false);
+
+  if (!microphonePermission) {
+    console.log(microphonePermission)
+    askMicrophonePermission()
+  }
+
+  const startLocalStream = async () => {
+    const constraints = {
+      audio: true,
+    };
+    const newStream = await mediaDevices.getUserMedia(constraints);
+    setLocalStream(newStream);
+    console.log('Started new stream:', newStream)
+  };
+
+  const startCall = async () => {
+    // You'll most likely need to use a STUN server at least. Look into TURN and decide if that's necessary for your project
+    const configuration = {iceServers: [{url: 'stun:stun.l.google.com:19302'}]};
+    const localPC = new RTCPeerConnection(configuration);
+    const remotePC = new RTCPeerConnection(configuration);
+
+    // could also use "addEventListener" for these callbacks, but you'd need to handle removing them as well
+    localPC.onicecandidate = e => {
+      try {
+        console.log('localPC icecandidate:', e.candidate);
+        if (e.candidate) {
+          remotePC.addIceCandidate(e.candidate);
+        }
+      } catch (err) {
+        console.error(`Error adding remotePC iceCandidate: ${err}`);
+      }
+    };
+    localPC
+    remotePC.onicecandidate = e => {
+      try {
+        console.log('remotePC icecandidate:', e.candidate);
+        if (e.candidate) {
+          localPC.addIceCandidate(e.candidate);
+        }
+      } catch (err) {
+        console.error(`Error adding localPC iceCandidate: ${err}`);
+      }
+    };
+    remotePC.onaddstream = e => {
+      console.log('remotePC tracking with ', e);
+      if (e.stream && remoteStream !== e.stream) {
+        console.log('RemotePC received the stream', e.stream);
+        setRemoteStream(e.stream);
+      }
+    };
+
+    // AddTrack not supported yet, so have to use old school addStream instead
+    // newStream.getTracks().forEach(track => localPC.addTrack(track, newStream));
+    localPC.addStream(localStream);
+    try {
+      const offer = await localPC.createOffer();
+      console.log('Offer from localPC, setLocalDescription');
+      await localPC.setLocalDescription(offer);
+      console.log('remotePC, setRemoteDescription');
+      await remotePC.setRemoteDescription(localPC.localDescription);
+      console.log('RemotePC, createAnswer');
+      const answer = await remotePC.createAnswer();
+      console.log(`Answer from remotePC: ${answer.sdp}`);
+      console.log('remotePC, setLocalDescription');
+      await remotePC.setLocalDescription(answer);
+      console.log('localPC, setRemoteDescription');
+      await localPC.setRemoteDescription(remotePC.localDescription);
+    } catch (err) {
+      console.error(err);
+    }
+    setCachedLocalPC(localPC);
+    setCachedRemotePC(remotePC);
+  };
+
+  // Mutes the local's outgoing audio
+  const toggleMute = () => {
+    if (!remoteStream) return;
+    localStream.getAudioTracks().forEach(track => {
+      console.log(track.enabled ? 'muting' : 'unmuting', ' local track', track);
+      track.enabled = !track.enabled;
+      setIsMuted(!track.enabled);
+    });
+  };
+
+  const closeStreams = () => {
+    if (cachedLocalPC) {
+      cachedLocalPC.removeStream(localStream);
+      cachedLocalPC.close();
+    }
+    if (cachedRemotePC) {
+      cachedRemotePC.removeStream(remoteStream);
+      cachedRemotePC.close();
+    }
+    setLocalStream();
+    setRemoteStream();
+    setCachedRemotePC();
+    setCachedLocalPC();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {!localStream && <Button title="Click to start stream" onPress={startLocalStream} />}
+      {localStream && <Button title="Click to start call" onPress={startCall} disabled={!!remoteStream} />}
+
+      {localStream && (
+        <View style={styles.toggleButtons}>
+          <Button title={`${isMuted ? 'Unmute' : 'Mute'} stream`} onPress={toggleMute} disabled={!remoteStream} />
+        </View>
+      )}
+      <Button title="Click to stop call" onPress={closeStreams} disabled={!remoteStream} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#313131',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    height: '100%',
+  },
+  text: {
+    fontSize: 30,
+  },
+  rtcview: {
     justifyContent: 'center',
+    alignItems: 'center',
+    height: '40%',
+    width: '80%',
+    backgroundColor: 'black',
+  },
+  rtc: {
+    width: '80%',
+    height: '100%',
+  },
+  toggleButtons: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 });
